dfa02ed57fea8b412b579046f7ebca184431db3d
Chrsteffen <Chrsteffen1@gmail.com>
2023-06-19 21:46:56 -0400

    first commit

---
+++ src/NavBar.tsx
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+
+function  NavBar(){
+  return ( 
+    <>
+      <nav>
+        <a>Home</a>
+        <a>Schedual</a>
+        <a>Vods</a>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
